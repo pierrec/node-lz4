6f1e7c3cd63a8afa07fc1a45214d0c580e0f0214
Pierre Curto <pierre.curto@gmail.com>
2013-03-13 12:51:21 +0100

    Updated examples

--- examples/file_compressHCSync.js
+++ examples/file_compressHCSync.js
@@ -13,10 +13,17 @@ var outputFile = process.argv[3] || inputFile + lz4.extension
 // Load the data
 var input = fs.readFileSync( inputFile )
 
-console.log('Compressing', inputFile, 'to', outputFile, '...')
-console.time('lz4')
+// Timing
+console.log('Compressing %s to %s...', inputFile, outputFile)
+var startTime = Date.now()
 var compressed = lz4.encode(input, true)
-console.timeEnd('lz4')
+var fileSize = fs.statSync(inputFile).size
+var delta = Date.now() - startTime
+console.log(
+	'lz4 compressing time: %dms (%dMb/s)'
+,	delta
+,	Math.round( 100 * fileSize / ( delta * (1 << 20) ) * 1000 ) / 100
+)
 
 // Save the uncompressed data
 fs.writeFileSync( outputFile, compressed )
--- examples/file_uncompressSync.js
+++ examples/file_uncompressSync.js
@@ -17,10 +17,18 @@ var input = fs.readFileSync( inputFile )
 // for faster decoding (no time spent resizing the output buffer)
 var outputSize = (128 << 20) // 128Mb
 
-console.log('Uncompressing', inputFile, 'to', outputFile, '...')
-console.time('lz4')
+// Timing
+var startTime = Date.now()
+console.log('Uncompressing %s to %s...', inputFile, outputFile)
 var decoded = lz4.decode( input, outputSize )
-console.timeEnd('lz4')
+
+var fileSize = fs.statSync(inputFile).size
+var delta = Date.now() - startTime
+console.log(
+	'lz4 decompressing time: %dms (%dMb/s)'
+,	delta
+,	Math.round( 100 * fileSize / ( delta * (1 << 20) ) * 1000 ) / 100
+)
 
 // Save the uncompressed data
 fs.writeFileSync( outputFile, decoded )
