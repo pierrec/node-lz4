8aa6cd8af6cff263f3a0a7d1815c525e09dd5194
Matthieu Bec <36496995+mbec-lbto@users.noreply.github.com>
2019-05-08 01:01:20 -0700

    Fix: Cannot find module 'lz4' (#79)
    * fix browserification using a softlink file with different name
    
    * update pre-built

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-browserify -o build/lz4.js -r ./lib/utils-js.js:./utils -r buffer -r ./lib/lz4.js:lz4 lib/lz4.js
+browserify -o build/lz4.js -r ./lib/utils-js.js:./utils -r buffer -r ./lib/lz4l.js:lz4 lib/lz4.js
 minify build/lz4.js > build/lz4.min.js
