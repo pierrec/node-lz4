4c6afea60bce6f7594a0f9746f8646b3c4ca4c4b
Matthieu Bec <mbec@lbto.org>
2019-05-12 14:26:54 -0700

    20190512 npm updates

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-browserify -o build/lz4.js -r ./lib/utils-js.js:./utils -r buffer -r ./lib/lz4l.js:lz4 lib/lz4.js
+browserify  -r ./lib/utils-js.js:./utils -r buffer -r xxhashjs -r ./lib/lz4l.js:lz4 -o build/lz4.js lib/lz4l.js && \
 minify build/lz4.js > build/lz4.min.js
+
