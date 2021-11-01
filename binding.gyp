c4712af4dd96c9e8fb1db5bc2d464ecc183ef128
maxeljkin <maxeljkin@gmail.com>
2021-11-02 00:27:03 +0300

    convert to node-addon-api

--- binding.gyp
+++ binding.gyp
@@ -2,18 +2,36 @@
   'targets': [
     {
       'target_name': 'xxhash',
+      'cflags!': [ '-fno-exceptions' ],
+      'cflags_cc!': [ '-fno-exceptions' ],
+      'xcode_settings': { 'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
+        'CLANG_CXX_LIBRARY': 'libc++',
+        'MACOSX_DEPLOYMENT_TARGET': '10.7',
+      },
+      'msvs_settings': {
+        'VCCLCompilerTool': { 'ExceptionHandling': 1 },
+      },
       'sources': [
         'lib/binding/xxhash_binding.cc',
         'deps/lz4/lib/xxhash.h',
         'deps/lz4/lib/xxhash.c',
       ],
       'include_dirs': [
-        '<!(node -e "require(\'nan\')")'
+        '<!(node -p "require(\'node-addon-api\').include_dir")',
       ],
       'cflags': [ '-O3' ],
     },
     {
       'target_name': 'lz4',
+      'cflags!': [ '-fno-exceptions' ],
+      'cflags_cc!': [ '-fno-exceptions' ],
+      'xcode_settings': { 'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
+        'CLANG_CXX_LIBRARY': 'libc++',
+        'MACOSX_DEPLOYMENT_TARGET': '10.7',
+      },
+      'msvs_settings': {
+        'VCCLCompilerTool': { 'ExceptionHandling': 1 },
+      },
       'sources': [
         'lib/binding/lz4_binding.cc',
         'deps/lz4/lib/lz4.h',
@@ -22,7 +40,7 @@
         'deps/lz4/lib/lz4hc.c',
       ],
       'include_dirs': [
-        '<!(node -e "require(\'nan\')")'
+        '<!(node -p "require(\'node-addon-api\').include_dir")',
       ],
       'cflags': [ '-O3' ],
     },
--- lib/binding/xxhash_binding.cc
+++ lib/binding/xxhash_binding.cc
@@ -1,122 +1,117 @@
-#include <string.h>
-#include <stdlib.h>
-
-#include <node.h>
-#include <node_buffer.h>
-#include <nan.h>
+#define NAPI_VERSION 3
+#include <napi.h>
 
 #define XXH_PRIVATE_API
-#include "../../deps/lz4/lib/xxhash.h"
 
-using namespace node;
-using namespace v8;
+#include "../../deps/lz4/lib/xxhash.h"
 
 //-----------------------------------------------------------------------------
 // xxHash
 //-----------------------------------------------------------------------------
 // {Buffer} input, {Integer} seed (optional)
-NAN_METHOD(xxHash) {
-  Nan::HandleScope scope;
+Napi::Value xxHash(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
 
   if (info.Length() == 0) {
-    Nan::ThrowError("Wrong number of arguments");
-    return;
+    Napi::Error::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return env.Null();
   }
 
-  if (!Buffer::HasInstance(info[0])) {
-    Nan::ThrowTypeError("Wrong argument: Buffer expected");
-    return;
+  if (!info[0].IsBuffer()) {
+    Napi::TypeError::New(env, "Wrong argument: Buffer expected").ThrowAsJavaScriptException();
+    return env.Null();
   }
 
-  Local<Object> input = Local<Object>::Cast(info[0]);
+  Napi::Buffer<char> input = info[0].As<Napi::Buffer<char>>();
   uint32_t seed = 0;
-  if (info[1]->IsUint32()) {
-    seed = info[1]->Uint32Value(Nan::GetCurrentContext()).FromJust();
+  if (info[1].IsNumber()) {
+    seed = info[1].As<Napi::Number>().Uint32Value();
   }
 
-  Local<Integer> result = Nan::New<Integer>(XXH32(Buffer::Data(input),
-                                                Buffer::Length(input),
-                                                seed)
-                                         );
-  info.GetReturnValue().Set(result);
+  Napi::Number result = Napi::Number::New(env, XXH32(input.Data(),
+                                                     input.Length(),
+                                                     seed)
+  );
+  return result;
 }
 
 // {Integer} seed
-NAN_METHOD(xxHash_init) {
-  Nan::HandleScope scope;
+Napi::Value xxHash_init(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
 
   if (info.Length() == 0) {
-    Nan::ThrowError("Wrong number of arguments");
-    return;
+    Napi::Error::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return env.Null();
   }
 
-  if (!info[0]->IsUint32()) {
-    Nan::ThrowTypeError("Wrong argument: Integer expected");
-    return;
+  if (!info[0].IsNumber()) {
+    Napi::TypeError::New(env, "Wrong argument: Integer expected").ThrowAsJavaScriptException();
+    return env.Null();
   }
 
-  uint32_t seed = info[0]->Uint32Value(Nan::GetCurrentContext()).FromJust();
+  uint32_t seed = info[0].As<Napi::Number>().Uint32Value();
 
   XXH32_state_t* state = XXH32_createState();
   XXH32_reset(state, seed);
-  Nan::MaybeLocal<Object> handle = Nan::NewBuffer((char *)state, sizeof(XXH32_state_t));
+  Napi::Buffer<char> handle = Napi::Buffer<char>::New(env, (char *) state, sizeof(XXH32_state_t));
 
-  info.GetReturnValue().Set(handle.ToLocalChecked());
+  return handle;
 }
 
 // {Buffer} state {Buffer} input {Integer} seed
-NAN_METHOD(xxHash_update) {
-  Nan::HandleScope scope;
+Napi::Value xxHash_update(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
 
   if (info.Length() != 2) {
-    Nan::ThrowError("Wrong number of arguments");
-    return;
+    Napi::Error::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return env.Null();
   }
 
-  if (!Buffer::HasInstance(info[0]) || !Buffer::HasInstance(info[1])) {
-    Nan::ThrowTypeError("Wrong arguments");
-    return;
+  if (!info[0].IsBuffer() || !info[1].IsBuffer()) {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
   }
+
+  Napi::Buffer<char> data0 = info[0].As<Napi::Buffer<char>>();
+  Napi::Buffer<char> data1 = info[1].As<Napi::Buffer<char>>();
 
   int err_code = XXH32_update(
-    (XXH32_state_t*) Buffer::Data(info[0]),
-    Buffer::Data(info[1]),
-    Buffer::Length(info[1])
+          (XXH32_state_t *) data0.Data(),
+          data1.Data(),
+          data1.Length()
   );
 
-  info.GetReturnValue().Set(Nan::New<Integer>(err_code));
+  return Napi::Number::New(env, err_code);
 }
 
 // {Buffer} state
-NAN_METHOD(xxHash_digest) {
-  Nan::HandleScope scope;
+Napi::Value xxHash_digest(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
 
   if (info.Length() != 1) {
-    Nan::ThrowError("Wrong number of arguments");
-    return;
+    Napi::Error::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return env.Null();
   }
 
-  if (!Buffer::HasInstance(info[0])) {
-    Nan::ThrowTypeError("Wrong arguments");
-    return;
+  if (!info[0].IsBuffer()) {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
   }
 
-  Local<Integer> res = Nan::New<Integer>(
-    XXH32_digest( (XXH32_state_t*) Buffer::Data(info[0]) )
+  Napi::Number res = Napi::Number::New(env,
+                                       XXH32_digest((XXH32_state_t *) info[0].As<Napi::Buffer<char>>().Data())
   );
 
-  info.GetReturnValue().Set(res);
+  return res;
 }
 
-NAN_MODULE_INIT(init_xxhash) {
-  Nan::Export(target, "xxHash", xxHash);
-  Nan::Export(target, "init", xxHash_init);
-  Nan::Export(target, "update", xxHash_update);
-  Nan::Export(target, "digest", xxHash_digest);
+Napi::Object init_xxhash(Napi::Env env, Napi::Object exports) {
+  exports.Set(Napi::String::New(env, "xxHash"), Napi::Function::New(env, xxHash));
+  exports.Set(Napi::String::New(env, "init"), Napi::Function::New(env, xxHash_init));
+  exports.Set(Napi::String::New(env, "update"), Napi::Function::New(env, xxHash_update));
+  exports.Set(Napi::String::New(env, "digest"), Napi::Function::New(env, xxHash_digest));
+
+  return exports;
 }
 
-#if (NODE_MAJOR_VERSION >= 10 && NODE_MINOR_VERSION >= 7) || NODE_MAJOR_VERSION >= 11
-  NAN_MODULE_WORKER_ENABLED(xxhash, init_xxhash)
-#else
-  NODE_MODULE(xxhash, init_xxhash)
-#endif
+NODE_API_MODULE(xxhash, init_xxhash);
