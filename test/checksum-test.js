1bcb05f952275e3d78f75c51b4b1e30eca200553
maximelkin <maxelkin@list.ru>
2021-11-05 19:29:52 +0300

    fix encoder stream blockChecksum & streamChecksum on empty input in utils-js (#111)
    Co-authored-by: maxeljkin <maxeljkin@gmail.com>

--- test/checksum-test.js
+++ test/checksum-test.js
@@ -1,36 +1,35 @@
-var fs = require('fs')
 var assert = require('assert')
 
 
 describe('LZ4 checksum', function () {
   var lz4 = require('..')
 
-  it('should encode/decode data', function (done) {
+  it('should encode/decode data', function () {
     var data = Buffer.alloc(200)
     data.fill(0)
 
-    lz4.decode(lz4.encode(data))
-
-    done()
+    assert.deepEqual(lz4.decode(lz4.encode(data, { blockChecksum: true })), data)
   })
 
-  it('should encode/decode data', function (done) {
+  it('should encode/decode data', function () {
     var data = Buffer.alloc(200)
     data.fill(16)
 
-    lz4.decode(lz4.encode(data))
-
-    done()
+    assert.deepEqual(lz4.decode(lz4.encode(data, { blockChecksum: true })), data)
   })
 
-  it('should encode/decode data', function (done) {
+  it('should encode/decode data', function () {
     var data = Buffer.alloc(200)
     data.slice(0, 100).fill(0)
     data.slice(100).fill(16)
 
-    lz4.decode(lz4.encode(data))
+    assert.deepEqual(lz4.decode(lz4.encode(data, { blockChecksum: true })), data)
+  })
 
-    done()
+  it('should encode/decode data #107', function () {
+    const data = Buffer.from('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+
+    assert.deepEqual(lz4.decode(lz4.encode(data, { blockChecksum: true })), data)
   })
 
 })
